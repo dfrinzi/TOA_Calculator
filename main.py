5b5e17ca5c074fd756677e456012a588987c942a
Dominic Frinzi <dominic.frinzi@basinprecision.com>
2024-02-28 20:10:37 -0600

    base calculation complete

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import turtle
+from tancalc import TanCalc
 
+tancalc = TanCalc()
+screen = turtle.Screen()
+screen.title("TOA Calculator For Valve Cavities")
+
+while True:
+    diameter1 = float(screen.textinput(title="Enter Cavity Details", prompt="First Diameter:"))
+    screen.tracer(1)
+    diameter2 = float(screen.textinput(title="Enter Cavity Details", prompt="Second Diameter:"))
+    screen.tracer(1)
+    angle = float(screen.textinput(title="Enter Cavity Details", prompt="Angle:"))
+    screen.tracer(1)
+    bottom_depth = float(screen.textinput(title="Enter Cavity Details", prompt="BOA Depth:"))
+    screen.tracer(1)
+    tancalc.top_depth_calc(angle=angle, diameter1=diameter1, diameter2=diameter2, bottom_depth=bottom_depth)
+
+
+turtle.mainloop()
---
+++ tancalc.py
@@ -0,0 +1,33 @@
+import math
+
+# decimal places in result, mm/2 is default
+MM_PRECISION = 2
+IN_PRECISION = 3
+PI = math.pi
+
+class TanCalc:
+    def __init__(self, unit="MM"):
+        self.unit = unit
+        self.precision = MM_PRECISION
+
+    def top_depth_calc(self, angle, diameter1, diameter2, bottom_depth,):
+        angle = abs(angle)
+        diameter1 = abs(diameter1)
+        diameter2 = abs(diameter2)
+        bottom_depth = abs(bottom_depth)
+
+        if self.unit == "IN":
+            self.precision = 3
+
+        tan_result = round(math.tan(angle * PI / 180), 10)
+        radial_change = abs(diameter1 - diameter2) * .5
+        depth_change = radial_change / tan_result
+        top_depth = round(bottom_depth - depth_change, self.precision)
+
+        print(f"tan_result: {round(tan_result, 4)}")
+        print(f"radial_change: {round(radial_change, 4)}")
+        print(f"depth_change: {round(depth_change, 4)}")
+        print(f"top_depth: {round(top_depth, 4)}")
+
+        return top_depth
+
