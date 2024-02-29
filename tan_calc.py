7d26bd1b9e0dd602212d14f91fd092caed9388b1
Dominic Frinzi <dominic.frinzi@basinprecision.com>
2024-02-28 19:29:05 -0600

    initial

---
+++ tan_calc.py
@@ -0,0 +1,10 @@
+
+
+
+class Tan_Calc:
+    def __init__(self, angle, diameter1, diameter2):
+        self.angle = angle
+        self.diameter1 = diameter1
+        self.diameter2 = diameter2
+        self.height = 0
+
